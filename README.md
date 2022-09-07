7effac610f39dade36fcb5f4591cd6648b40079e
Udymilla Chagas <81454682+Udymilla@users.noreply.github.com>
2022-09-07 15:54:35 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mentalista
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/udymilla/pen/LYmpKya](https://codepen.io/udymilla/pen/LYmpKya).
+
+Mentalista é um cosole desenvolvido durante a IversãoDev da Alura, onde o objetivo é adivinhar o numero secreto, desenvolvido por meio da lógica da programação, com HTML, CSS e JS
---
+++ script.js
@@ -0,0 +1,18 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+ function Chutar() {
+   var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  console.log(chute);
+    
+  if(chute == numeroSecreto){
+    elementoResultado.innerHTML="Acertou!";
+  }
+  else if ( chute > 10 || chute < 0){
+    elementoResultado.innerHTML="Voce deve digitar um número entre 0 e 10";
+  }
+    else {
+    elementoResultado.innerHTML="Errou!" + numeroSecreto "era o numeor secreto";
+    }
+   
+}
